87ae3e68f9568329fa7711ded853ca0dcd05822a
Su Jung Youn <suyoun@c3r4p6.42vienna.com>
2026-07-18 18:30:50 +0200

    header path changed

--- push_swap.h
+++ push_swap.h
@@ -6,15 +6,15 @@
 /*   By: suyoun <suyoun@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/18 17:17:35 by bsurilla          #+#    #+#             */
-/*   Updated: 2026/07/18 18:03:56 by suyoun           ###   ########.fr       */
+/*   Updated: 2026/07/18 18:30:36 by suyoun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include "../libft/includes/libft.h"
-#include "../ft_printf/includes/ft_printf.h"
+#include "../libft/libft.h"
+#include "../ft_printf/ft_printf.h"
 
 # include <unistd.h>
 # include <stdlib.h>
