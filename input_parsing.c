f2eedc4af18b1f0bae2bcde30b6263da1e10cbd4
Su Jung Youn <suyoun@c3r4p6.42vienna.com>
2026-07-19 17:27:16 +0200

    for the parsing, skeleton down

---
+++ input_parsing.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_parsing.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: suyoun <suyoun@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/19 14:44:13 by suyoun            #+#    #+#             */
+/*   Updated: 2026/07/19 17:18:21 by suyoun           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+char	**get_numbers(int argc, char **argv);
+
+void parse_input(int argc, char **argv)
+{
+	char **numbers;
+
+	numbers = NULL;
+
+	if (argc == 2)
+	{
+		numbers = ft_split (argv[1], ' ');
+	}
+	else if
+	{
+
+		
+	}
+}
+
+int	valid_num();
+int	dup
