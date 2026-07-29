1c39df3f262a037c724d7536863f32c1f32ebab4
Sue <ssuess.tv@gmail.com>
2026-07-29 15:53:06 +0200

    selection sort

--- selection_sort.c
+++ selection_sort.c
@@ -6,9 +6,21 @@
 /*   By: suyoun <suyoun@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/29 15:24:13 by suyoun            #+#    #+#             */
-/*   Updated: 2026/07/29 15:34:49 by suyoun           ###   ########.fr       */
+/*   Updated: 2026/07/29 15:52:48 by suyoun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	selection_sort(int *arr, int size)
+{
+	int i;
+	int smallest;
+
+	i = 0;
+	smallest = arr[0];
+	while (i < size)
+	{
+		
+	}
+}
