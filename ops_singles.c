279351759ac2495aecadd04a86b20d7afab12cae
bsurilla <bea_surilla@ymail.com>
2026-08-11 09:00:19 +0200

    removed push capacity check

--- ops_singles.c
+++ ops_singles.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ops_singles                                        :+:      :+:    :+:   */
+/*   ops_singles.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bsurilla <bsurilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/10 20:21:06 by bsurilla          #+#    #+#             */
-/*   Updated: 2026/08/10 22:12:04 by bsurilla         ###   ########.fr       */
+/*   Updated: 2026/08/11 08:54:50 by bsurilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,6 @@ void	push(t_stack *stack1, t_stack *stack2)
 	if (stack2->size == 0)
 		return ;
 	i = stack1->size;
-	if (i > stack2->capacity)
-		return ;
 	while (i > 0)
 	{
 		stack1->numbers[i] = stack1->numbers[i - 1];
@@ -53,10 +51,10 @@ void	rotate(t_stack *stack)
 	int	temp;
 	int	i;
 	
+	if (!stack || stack->size < 1)
+	return ;
 	i = 0;
 	temp = stack->numbers[0];
-	if (!stack || stack->size < 1)
-		return ;
 	while(i < stack->size - 1)
 	{
 		stack->numbers[i] = stack->numbers[i + 1];
@@ -70,12 +68,14 @@ void	reverse_rotate(t_stack *stack)
 	int	temp;
 	int i;
 	
-	i = stack->size - 1;
-	temp = stack->numbers[i];
 	if (!stack || stack->size < 1)
 		return ;
+	i = stack->size - 1;
+	temp = stack->numbers[i];
 	while(i > 0)
+	{
 		stack->numbers[i] = stack->numbers[i - 1];
 		i--;
+	}
 	stack->numbers[0] = temp;
 }
