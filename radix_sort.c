40fda0f7f08fe7c9d0416e2f07c97a9a00b1fb71
Sue <ssuess.tv@gmail.com>
2026-08-16 19:39:24 +0200

    radix in progress

--- radix_sort.c
+++ radix_sort.c
@@ -6,27 +6,27 @@
 /*   By: suyoun <suyoun@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 18:42:53 by suyoun            #+#    #+#             */
-/*   Updated: 2026/08/05 01:35:14 by suyoun           ###   ########.fr       */
+/*   Updated: 2026/08/16 19:36:13 by suyoun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void radix_sort(t_stack stack_a)
+void radix_sort(t_stack *stack_a, t_stack *stack_b)
 {
-
+	
 }
 
-int	*copy_array(t_stack *stack)
+int *copy_array(int *array, int size)
 {
 	int	*copied;
 	int	i;
 
-	copied = malloc(stack->size * sizeof(int));
+	copied = malloc(size * sizeof(int));
 	if (!copied)
 		return (NULL);
 	i = 0;
-	while (i < stack->size)
+	while (i < size)
 	{
-		copied[i] = stack->numbers[i];
+		copied[i] = array[i];
 		i++;
 	}
 	return (copied);
@@ -34,8 +34,13 @@ int	*copy_array(t_stack *stack)
 
 void normalize(int *array, int size)
 {
-	int *copid;
+	int *copied;
+	t_stack temp;
 
-	copied = copy_array(original);
-	selection_sort(copied, );
+	copied = copy_array(array, size);
+	temp.numbers = copied;
+	temp.size = size;
+	selection_sort(&temp, size);
 }
+
+void selection_sort_array(int *array, int size);
