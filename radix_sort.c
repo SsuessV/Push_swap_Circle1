04357e9e942727ec3db6f9d62651f78cccbeb05d
Sue <ssuess.tv@gmail.com>
2026-08-17 14:45:27 +0200

    radix sort done (hopefully)

--- radix_sort.c
+++ radix_sort.c
@@ -6,14 +6,50 @@
 /*   By: suyoun <suyoun@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 18:42:53 by suyoun            #+#    #+#             */
-/*   Updated: 2026/08/17 10:05:21 by suyoun           ###   ########.fr       */
+/*   Updated: 2026/08/17 14:38:18 by suyoun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void radix_sort(t_stack *stack_a, t_stack *stack_b)
+void	radix_sort(t_stack *stack_a, t_stack *stack_b)
 {
+	int	bit;
+	int	max_bit;
+	int	i;
+	int	size;
+
 	assign_ranks(stack_a);
-	
+	size = stack_a->size;
+	max_bit = count_bits(size);
+	bit = 0;
+	while (bit < max_bit)
+	{
+		i = 0;
+		while (i < size)
+		{
+			if ((stack_a->numbers[0].rank >> bit & 1) == 0)
+				push(stack_b, stack_a);
+			else
+				rotate(stack_a);
+			i++;
+		}
+		while (stack_b->size > 0)
+			push(stack_a, stack_b);
+		bit++;
+	}
+}
+
+int	count_bits(int size)
+{
+	int	bit;
+
+	bit = 0;
+	size--;
+	while (size > 0)
+	{
+		size >>= 1;
+		bit++;
+	}
+	return (bit);
 }
